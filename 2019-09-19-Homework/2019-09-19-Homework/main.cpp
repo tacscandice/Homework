3291b9f91e7d26cc05dd5a489595a79bd29fa63b
tacscandice <55443460+tacscandice@users.noreply.github.com>
2019-09-19 08:36:17 -0400

    Add files via upload

---
+++ 2019-09-19-Homework/2019-09-19-Homework/main.cpp
@@ -0,0 +1,22 @@
+// Name: Candice Duncan
+// Intro to Computer Science
+// Period 1
+// Mr. Williams
+// 2019-09-19-Homework
+
+#include <iostream>
+
+int main()
+{
+	std::cout	<< "Name: Candice Duncan" << std::endl
+				<<"Period 1: Intro To Computer Science" << std::endl
+				<<"Period 2: Health" << std::endl
+				<<"Period 3: Physical Education" << std::endl
+				<<"Period 4: House" << std::endl
+				<<"Period 5: Spanish 2" << std::endl
+				<<"Period 6: English 10 Honors" << std::endl
+				<<"Period 7: Lunch" << std::endl
+				<<"Period 8: Global History & Geography 1" << std::endl
+				<<"Period 9: Algebra 2" << std::endl
+				<<"Period 10: Earth Science"; 
+}
